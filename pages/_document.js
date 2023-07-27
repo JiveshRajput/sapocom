fbf1f149a205d01e2e3cdfc7941295e4bf206e78
jivesh <jsoperatorz@gmail.com>
2023-07-27 20:52:13 +0530

    Added: Google Analytics code

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,17 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YHB4RGD380"></script>
+        <script>
+          {` window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YHB4RGD380');
+          gtag('config', 'AW-11258657664');`}
+        </script>
+      </Head>
       <body>
         <Main />
         <NextScript />
