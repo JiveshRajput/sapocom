1f328f4c6e4b21426e9c671daf8b7e5b14f8b28c
Aadhar Chandiwala <chandiwalaaadhar@gmail.com>
2023-08-03 12:15:20 +0000

    Added Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,21 @@ import '../styles/fonts.css'
 
 export default function App({ Component, pageProps }) {
   return <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-71N5J1ZFMK`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-71N5J1ZFMK', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
     <Provider store={store}> 
       <Layout>
         <Component {...pageProps} />
