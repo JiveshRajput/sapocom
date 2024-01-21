797404421375236b124d338f025c3bbeb4ce92db
Dhruv <mahajandhruv420@gmail.com>
2024-01-21 17:02:18 +0530

    minor fixes

--- pages/careers/index.js
+++ pages/careers/index.js
@@ -78,15 +78,21 @@ export default function Careers() {
       {/* Pathway for interns */}
       <div className="w-full pb-12 md:pb-20">
         <div className="max-w-1200 mx-auto flex flex-wrap-reverse px-2 items-center justify-center">
-          <Image src={img1} className="w-[350px]" />
+          <Image src={img1} alt={"careers-img-2"} className="w-[350px]" />
           <div className="max-w-[600px] mx-auto p-2">
             <h1 className="pb-4 text-left text-4xl font-bold">
               Paving global pathway for Interns
             </h1>
             <p className="text-left text-base text-gray-600">
-            Embark on a transformative journey with our internship program as we invite aspiring talents to join our dynamic team. This isn't just an internship; it's an opportunity to unlock your potential and shape your career trajectory on a global scale. 
- our internship program is designed to be a launchpad for your professional aspirations. From hands-on projects to mentorship opportunities, we offer an environment that fosters learning and growth. Your journey starts here, and the future is yours to shape! 
-Apply now and be a part of something extraordinary.
+              Embark on a transformative journey with our internship program as
+              we invite aspiring talents to join our dynamic team. This isn&apos;t
+              just an internship; it&apos;s an opportunity to unlock your potential
+              and shape your career trajectory on a global scale. our internship
+              program is designed to be a launchpad for your professional
+              aspirations. From hands-on projects to mentorship opportunities,
+              we offer an environment that fosters learning and growth. Your
+              journey starts here, and the future is yours to shape! Apply now
+              and be a part of something extraordinary.
             </p>
           </div>
         </div>
@@ -96,10 +102,22 @@ Apply now and be a part of something extraordinary.
               For Experienced Professionals
             </h1>
             <p className="text-left text-base text-gray-600">
-            Welcome to our global career hub, where diverse talents converge to shape the future. At SAPOCOM, we celebrate the richness of cultures and perspectives that make up our worldwide team. As you navigate through our career opportunities, envision joining a dynamic environment that spans continents, embraces diversity, and thrives on collaboration. Our commitment to innovation knows no borders, and we invite professionals from every corner of the globe to contribute their unique skills. Whether you're an experienced global executive or a recent graduate with a universal mindset, we provide a platform for you to bring your talents to a truly international stage.  Explore the opportunities, discover your potential, and become part of a cosmopolitan community that's shaping the future at SAPOCOM
+              Welcome to our global career hub, where diverse talents converge
+              to shape the future. At SAPOCOM, we celebrate the richness of
+              cultures and perspectives that make up our worldwide team. As you
+              navigate through our career opportunities, envision joining a
+              dynamic environment that spans continents, embraces diversity, and
+              thrives on collaboration. Our commitment to innovation knows no
+              borders, and we invite professionals from every corner of the
+              globe to contribute their unique skills. Whether you&apos;re an
+              experienced global executive or a recent graduate with a universal
+              mindset, we provide a platform for you to bring your talents to a
+              truly international stage. Explore the opportunities, discover
+              your potential, and become part of a cosmopolitan community that&apos;s
+              shaping the future at SAPOCOM
             </p>
           </div>
-          <Image src={img2} className="w-[350px]" />
+          <Image src={img2} alt={"careers-img-2"} className="w-[350px]" />
         </div>
       </div>
     </>
