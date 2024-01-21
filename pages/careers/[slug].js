d25069dd1fc2269b7cdc273a8adc8634ac1f86dc
Dhruv <mahajandhruv420@gmail.com>
2024-01-21 16:57:47 +0530

    done careers info page

--- pages/careers/[slug].js
+++ pages/careers/[slug].js
@@ -18,9 +18,7 @@ function MyPage() {
     const fetchData = async () => {
       try {
         dispatch(setLoadingState(true));
-        const response = await axios.get(
-          `/api/jobs?slug=${slug}`
-        );
+        const response = await axios.get(`/api/jobs?slug=${slug}`);
         setJobData(response.data[0]);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -36,7 +34,11 @@ function MyPage() {
     <>
       <Loader />
       <SetHeaders title={`${jobData.title} | Sapocom`} />
+
+      {/* Job info */}
       <JobCard jobData={jobData} />
+
+      {/* Job Requirements */}
       {jobData.details && (
         <div className="w-full p-4 py-8 md:py-10">
           <h2 className="max-w-1000 mx-auto text-2xl font-bold text-secondary pb-2">
@@ -48,6 +50,8 @@ function MyPage() {
           />
         </div>
       )}
+
+      {/* Back to careers page button */}
       <div className="mx-auto max-w-1200 p-4">
         <Link passHref href={"/careers"}>
           <button className="py-1 px-4 border-2 border-primary text-primary rounded-full font-semibold hover:bg-primary/10">
@@ -55,6 +59,8 @@ function MyPage() {
           </button>
         </Link>
       </div>
+      
+      {/* form to apply for job */}
       <JobForm jobData={jobData} />
     </>
   );
--- pages/careers/index.js
+++ pages/careers/index.js
@@ -5,6 +5,9 @@ import JobOpening from "@/components/JobOpening";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loader from "@/layouts/Loader";
+import Image from "next/image";
+import img1 from "@/assets/images/careers/career-info-img1.webp";
+import img2 from "@/assets/images/careers/career-info-img2.webp";
 import { useDispatch } from "react-redux";
 import { setLoadingState } from "@/store/reducers/valueReducer";
 export default function Careers() {
@@ -35,6 +38,7 @@ export default function Careers() {
     <>
       <Loader />
       <SetHeaders title="Careers | Sapocom" />
+
       {/* Banner */}
       <BannerWithClickHandler
         heading="Join Our Talented Team"
@@ -70,6 +74,34 @@ export default function Careers() {
           </div>
         </div>
       )}
+
+      {/* Pathway for interns */}
+      <div className="w-full pb-12 md:pb-20">
+        <div className="max-w-1200 mx-auto flex flex-wrap-reverse px-2 items-center justify-center">
+          <Image src={img1} className="w-[350px]" />
+          <div className="max-w-[600px] mx-auto p-2">
+            <h1 className="pb-4 text-left text-4xl font-bold">
+              Paving global pathway for Interns
+            </h1>
+            <p className="text-left text-base text-gray-600">
+            Embark on a transformative journey with our internship program as we invite aspiring talents to join our dynamic team. This isn't just an internship; it's an opportunity to unlock your potential and shape your career trajectory on a global scale. 
+ our internship program is designed to be a launchpad for your professional aspirations. From hands-on projects to mentorship opportunities, we offer an environment that fosters learning and growth. Your journey starts here, and the future is yours to shape! 
+Apply now and be a part of something extraordinary.
+            </p>
+          </div>
+        </div>
+        <div className="max-w-1200 mx-auto flex flex-wrap px-2 pt-8 md:pt-12 justify-center items-center">
+          <div className="max-w-[600px] mx-auto p-2 ">
+            <h1 className="pb-4 text-left text-4xl font-bold">
+              For Experienced Professionals
+            </h1>
+            <p className="text-left text-base text-gray-600">
+            Welcome to our global career hub, where diverse talents converge to shape the future. At SAPOCOM, we celebrate the richness of cultures and perspectives that make up our worldwide team. As you navigate through our career opportunities, envision joining a dynamic environment that spans continents, embraces diversity, and thrives on collaboration. Our commitment to innovation knows no borders, and we invite professionals from every corner of the globe to contribute their unique skills. Whether you're an experienced global executive or a recent graduate with a universal mindset, we provide a platform for you to bring your talents to a truly international stage.  Explore the opportunities, discover your potential, and become part of a cosmopolitan community that's shaping the future at SAPOCOM
+            </p>
+          </div>
+          <Image src={img2} className="w-[350px]" />
+        </div>
+      </div>
     </>
   );
 }
